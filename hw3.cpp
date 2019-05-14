fedc5a54091a594d7534f2b167ebce981f393eef
kawong215 <wong.karina20@gmail.com>
2019-05-14 11:41:52 -0700

    allocating memory at PAGE

--- hw3.cpp
+++ hw3.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 #include <fstream>
 #include <string> 
+#include <vector>
 using namespace std;
 
+struct page_table
+{
+	vector<int> process_id;
+	vector<int> virtual_addr;
+	vector<int> physical_addr;
+	vector<bool> dirty_bit;
+	vector<bool> swapped; 
+};
 
 int rows = 100;
 int columns = 3;
-
-int create();
+vector<int> physical; 
 
 int main() 
 {
@@ -17,6 +25,7 @@ int main()
 	int process[rows]; 
 	char action[rows];
 	int page[rows]; 
+	 
 
 	if (!infile)
 	{
@@ -40,6 +49,8 @@ int main()
 		count++; 
 	}
 
+	// for checking purposes
+	//cout << "Original count: " << count << endl; 
 	cout << "Number of rows: " << count-1 << endl; 
 
 	for (int i = 0; i < count-1; i++)
@@ -47,5 +58,34 @@ int main()
 		cout << process[i] << " " << action[i] << " " << page[i] << endl;
 	}
 
+	page_table pg; 
+
+	// put values in page table
+	for (int i = 0; i < count-1; i++)
+	{
+		if (action[i] == 'A')
+		{
+			physical.push_back(process[i]); 
+			pg.virtual_addr.push_back(page[i]); 
+		}
+	}
+
+	// for checking purposes
+	cout << "Physical pages allocated: " << endl; 
+	for (int i = 0; i < physical.size(); i++)
+	{
+		cout << "At index " << i << endl;
+		cout << physical[i] << " ";
+		cout << pg.virtual_addr[i] << endl; 
+		
+	}
+	
+	// check if physical pages are full
+	if (physical.size() == 20)
+	{
+		cout << "Physical pages are full" << endl; 
+	}
+
 	infile.close(); 
 }
+
